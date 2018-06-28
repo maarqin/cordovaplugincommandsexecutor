5d5ac8c8ddbaf27a3514311e4ac1152d83d9027f
Marcos Thomaz <marcos@iMac-de-Dev-Mobile.local>
2018-06-28 17:02:10 -0300

    first commit

---
+++ src/ios/CommandsExecutor.m
@@ -0,0 +1,28 @@
+/********* CommandsExecutor.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CommandsExecutor : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation CommandsExecutor
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
